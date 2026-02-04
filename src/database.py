0f2d13d6050fbd5edd5fc184b904fb6d11ffc9cc
Omkaar <omkaarkasodekar@gmail.com>
2026-02-04 15:43:28 +0530

    mysql to sqlite

--- src/database.py
+++ src/database.py
@@ -1,21 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "mysql+pymysql://mongouhd_evernorth:U*dgQkKRuEHe@cp-15.webhostbox.net/mongouhd_evernorth"
 
-if DATABASE_URL:
-    engine = create_engine(
-        DATABASE_URL,
-        pool_pre_ping=True,
-        pool_recycle=1800,
-        echo=False,
-    )
-else:
-    engine = create_engine(
-        "sqlite:///./test.db",
-        connect_args={"check_same_thread": False},
-        echo=False,
-    )
+engine = create_engine(
+    "sqlite:///./test.db",
+    connect_args={"check_same_thread": False},
+    echo=False,
+)
 
 SessionLocal = sessionmaker(
     bind=engine,
