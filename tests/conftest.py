ab0cc7136a28c24684d77ab502d251d2b18190c2
Omkaar <omkaarkasodekar@gmail.com>
2026-02-05 18:37:06 +0530

    updated toml

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,12 +11,14 @@ TEST_DATABASE_URL = "sqlite:///./test1.db"
 engine = create_engine(
     TEST_DATABASE_URL,
     connect_args={"check_same_thread": False},
+    future=True,
 )
 
 TestingSessionLocal = sessionmaker(
     bind=engine,
     autoflush=False,
     autocommit=False,
+    future=True,
 )
 
 
@@ -25,18 +27,16 @@ def client():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
-    db = TestingSessionLocal()
-
     def override_get_db():
+        db = TestingSessionLocal()
         try:
             yield db
         finally:
-            pass
+            db.close()
 
     app.dependency_overrides[get_db] = override_get_db
 
     with TestClient(app) as c:
         yield c
 
-    db.close()
     app.dependency_overrides.clear()
